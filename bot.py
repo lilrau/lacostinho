8311411e1e988709e42749b2024115c89417bde4
lilrau <91678438+lilrau@users.noreply.github.com>
2021-09-30 12:58:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,10 +49,28 @@ async def nenem(ctx):
 # comando purge
 @client.command(aliases=['purge'])
 @commands.has_permissions(manage_messages=True)
-async def clear(ctx, member):
+async def clear(ctx):
     await ctx.channel.purge(limit=1000000)
-    await ctx.send(f'Chat do canal limpo por {member.mention}.')
 
+
+# comando help
+@client.command()
+async def ajuda(ctx):
+    embedVar = discord.Embed(title="Comandos",
+                             description="",
+                             color=0x9600FF,
+                             url='https://github.com/lilrau/lacostinho/blob/main/comandos')
+    embedVar.add_field(name="help", value="Mostra esta mensagem.", inline=False)
+    embedVar.add_field(name="oi", value="Diga \"oi\" para o Lacostinho!", inline=False)
+    embedVar.add_field(name="gostosa", value="Mostra uma gostosa.", inline=False)
+    embedVar.add_field(name="nenem", value="Mostra um neném.", inline=False)
+    embedVar.add_field(name="purge", value="Limpa as mensagens do canal.", inline=False)
+    embedVar.set_footer(
+        text="Atualizado por raulzinho  •  30/09/2021",
+        icon_url='https://cdn.discordapp.com/attachments/709526517864202271/893127205822013520/image0.jpg')
+    embedVar.set_image(
+        url='https://cdn.discordapp.com/attachments/709526517864202271/893142073220403220/comandos_bot_wpp.png')
+    await ctx.send(embed=embedVar)
 
 
 client.run(TOKEN)
